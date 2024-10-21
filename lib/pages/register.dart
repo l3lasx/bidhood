a67732a55d56b9908797559f65da28cf8eed8963
Singharat (Sodality) <151146338+singharat4@users.noreply.github.com>
2024-10-21 23:12:22 +0700

    remove duplicate import

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -12,7 +12,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:dio/dio.dart';
 
 class RegisterPage extends ConsumerStatefulWidget {
   const RegisterPage({super.key});
